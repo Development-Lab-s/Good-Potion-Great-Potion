3ad8e7a557ea84f83511752aa0f2f6fa0e6c0816
common0902 <minsoung200@gmail.com>
2025-04-20 20:26:13 +0900

    feat : cook minigame
    바늘을 타이밍에 맞춰서 멈추는
    게임의 프로토타입을 만들었다.

---
+++ Assets/00.Work/Minsoung/02.Scripts/FinishLogic.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using static UnityEngine.Rendering.DebugUI;
+
+public class ClockHand : MonoBehaviour
+{
+    private TimerLogic _timerLogic;
+    private FinishRotation _finishRotation;
+    private bool _finishCheck = false;
+    private int _finishCount;
+
+    
+    private void Start()
+    {
+        _timerLogic = GameObject.FindAnyObjectByType<TimerLogic>();
+        _finishRotation = GameObject.FindAnyObjectByType<FinishRotation>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            _finishCheck = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            _finishCheck = false;
+        }
+    }
+    private void Update()
+    {
+        if (_finishCheck == true)
+        {
+            if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            {
+                _timerLogic.ClockHandDir();
+                _timerLogic.ClockHandSpeed();
+                SquareScale();
+                _finishCount++;
+                _finishRotation.FinishRotaton(true);
+            }
+        }
+        if (_finishCheck == false)
+        {
+            if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            {
+                Time.timeScale = 0;
+            }
+        }
+        if (_finishCount == 3)
+        {
+            Time.timeScale = 0;
+        }
+    }
+    private void SquareScale()
+    {
+        transform.localScale -= new Vector3(0.09f,0f,0f);
+    }
+}
