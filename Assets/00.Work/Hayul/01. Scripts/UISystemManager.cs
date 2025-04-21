0d2a457677cabc2f98452bd7fc239c71595789f8
hayull207 <ohyohy1207@gmail.com>
2025-04-21 23:03:38 +0900

    feat : StickDrag
    스틱이 마우스 드래그를 따라 움직임

---
+++ Assets/00.Work/Hayul/01. Scripts/UISystemManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class UISystemManager : MonoBehaviour
+{
+    [SerializeField] private GameObject LeftUI, RightUI;
+    private void Start()
+    {
+        if (Random.Range(0, 2) == 0)
+        {
+            OnLeftUI();
+        }
+        else
+        {
+            OnRightUI();
+        }
+        
+    }
+
+    private void OnLeftUI()
+    {
+        LeftUI.SetActive(true);
+    }
+
+    private void OnRightUI()
+    {
+        RightUI.SetActive(true);
+    }
+    
+
+}
