257cc92d4226c8690d856a3a59670d86d15f4a86
Nugusaeyo <chsj0702@gmail.com>
2025-04-22 23:50:43 +0900

    Merge Guidebook
    닷트윈 추가, GuideBookSO, TestSO추가

---
+++ Assets/00.Work/Nugusaeyo/Script/GuideBookSO.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName ="SO/GuideBookSO")]
+public class GuideBookSO : ScriptableObject
+{
+    public Sprite potionImage;
+    public string potionName;
+
+    public Sprite slotImage1;
+    public string slotName1;
+
+    public Sprite slotImage2;
+    public string slotName2;
+
+    public Sprite slotImage3;
+    public string slotName3;
+}
---
+++ Assets/00.Work/Nugusaeyo/Script/GuideUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GuideUI : MonoBehaviour
+{
+    [SerializeField] private  GuideBookSO _guideSO;
+
+    [SerializeField] private Image potionImg;
+    [SerializeField] private Image slotImg1;
+    [SerializeField] private Image slotImg2;
+    [SerializeField] private Image slotImg3;
+    private void Start()
+    {
+        potionImg.sprite = _guideSO.potionImage;
+
+        slotImg1.sprite = _guideSO.slotImage1;
+        slotImg2.sprite = _guideSO.slotImage2;
+        slotImg3.sprite = _guideSO.slotImage3;
+    }
+
+}
